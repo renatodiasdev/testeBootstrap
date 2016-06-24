8ad9c92fbadb650ca0edcbecd4a6e64406bab2c4
renatodiasdev <j_renatodias@hotmail.com>
2016-06-24 12:55:19 -0300

    criando o index.html

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    cssnano = require('gulp-cssnano');
 
 gulp.task('default', function () {
-    
+    return gulp.src('bower_components/bootstrap/dist/css/*.css')
+    .pipe(cssnano())
+    .pipe(gulp.dest('wwwroot/css/'));
 });
